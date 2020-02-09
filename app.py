b2f414ff6de4f12c22df6caf7649fd6771506cc4
VirHos <fomichev.n@gmail.com>
2020-02-09 22:03:02 +0300

    2 chapter working template

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template
+import data
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('index.html', data=data, tourslist=[4,5,3,2,7,9])
 
 @app.route('/from/<direction>')
 def fromdir(direction):
-    return render_template('direction.html')
+    return render_template('direction.html', data=data, direction=direction)
 
 @app.route('/tours/<id>/')
 def tours(id):
-    return render_template('tour.html')
+    return render_template('tour.html', data=data, id=int(id))
 
 
 app.run()
